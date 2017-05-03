6ad0aefbee76610135f95fd858a0434c3b249cfa
Melissa Munson <melissamunson@guest-2-00985.dhcp.unc.edu>
2017-05-02 20:23:08 -0400

    adding passport stuff to server file

--- server.js
+++ server.js
@@ -2,18 +2,26 @@
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
+var passport = require('passport');
+
+
+require('./config/passport')(passport);
 
 
 // Initialize Express
 var app = express();
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));
 
 // Database configuration
 // Save the URL of our database as well as the name of our collection
-var databaseUrl = "zoo";
-var collections = ["animals"];
+var databaseUrl = "barter";
+var collections = ["traders"];
 
 // Use mongojs to hook the database to the db variable
 var db = mongojs(databaseUrl, collections);
@@ -33,7 +41,7 @@ app.get("/", function(req, res) {
 // 2. At the "/all" path, display every entry in the barter collection
 app.get("/all", function(req, res) {
   // Query: In our database, go to the barter collection, then "find" everything
-  db.animals.find({}, function(error, found) {
+  db.traders.find({}, function(error, found) {
     // Log any errors if the server encounters one
     if (error) {
       console.log(error);
