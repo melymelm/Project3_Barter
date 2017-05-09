2db100c84822d03373ce6d5eb124c4c2fb5656b4
MelissaLoggins <melissaloggins@gmail.com>
2017-05-09 16:21:59 -0400

    displaying search results in real time

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
-
-  console.log("search.js: Inside ready function");
   function performSearch() {
   	var category = this.id;
-  	console.log("category " + category);
     $.get({
       url: '/categories/' + category,
-      success: function(data) {
-        console.log(data);
-        // updateCategories(data);
+      success: function(data) { 
+        updateSearchResults(data);
 
       }
     }) 
   }
 
   $( ".performSearch" ).click(performSearch)
+
+  function updateSearchResults (searchData){
+    var results = $('#searchResults');
+    results.empty();
+ 
+    searchData.forEach(function (result) {
+  	  var searchData = [result.businessName, result.fullName, result.phoneNumber, result.email];
+  	  var searchText = searchData.join(', ');
+        $('<li class="list-group-item"></li>').html(searchText).appendTo(results);
+    });
+  }
 });
+
+
