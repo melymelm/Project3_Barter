789c3b8fb5c6976909af65db1f89cbc714e9f8a9
MelissaLoggins <melissaloggins@gmail.com>
2017-05-09 17:02:10 -0400

    Google maps is up and running

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,3 +1,7 @@
+
+var map = null;
+
+
 $(document).ready(function() {
   function performSearch() {
   	var category = this.id;
@@ -20,8 +24,42 @@ $(document).ready(function() {
   	  var searchData = [result.businessName, result.fullName, result.phoneNumber, result.email];
   	  var searchText = searchData.join(', ');
         $('<li class="list-group-item"></li>').html(searchText).appendTo(results);
+
+        // TODO add a marker to google map for each search result
+        // First, use zipcode to find lat/long (either here or when user signs up)
+        /*
+          var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+        */
     });
   }
 });
 
 
+// Google maps
+
+function centerMap(latitude, longitude) {
+	map = new google.maps.Map(document.getElementById('map'), {
+  		center: {lat: latitude, lng: longitude},
+  		zoom: 8
+	});
+}
+
+function initMap() {
+	if ("geolocation" in navigator) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+  		centerMap(position.coords.latitude, position.coords.longitude);
+		});
+	} else {
+	  console.log('geolocation is unavailable');
+	}
+
+	setTimeout(function() {
+        if (!map) {
+          console.log("No confirmation from user, using fallback");
+          centerMap(35.7796, -78.6382); // Default to Raleigh, NC
+        }
+      }, 5000);
+}
