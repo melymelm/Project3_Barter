8142b29a40be5e8869e6a2c28f02fba41d3278bb
MelissaLoggins <melissaloggins@gmail.com>
2017-05-09 12:46:20 -0400

    i think it was a .list issue, believe it or not

--- server.js
+++ server.js
@@ -235,6 +235,7 @@ app.get("/traders/:id", function(req, res) {
 
 
 // Set the app to listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+app.listen(process.env.PORT || 5000)
+// app.listen(3000, function() {
+//   console.log("App running on port 3000!");
+// });
